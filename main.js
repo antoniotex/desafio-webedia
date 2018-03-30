558ed2ded64402b27f41a3d9a2373cd56087ac07
antoniotex <acm.skt@gmail.com>
2018-03-30 12:22:17 -0300

    Formatado artigos da pagina inicial

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 var url = 'https://newsapi.org/v2/top-headlines?' +
-          'country=pt&' +
+          'sources=mtv-news&' +
           'apiKey=0374b95ead8b4547b4180d4b531d9026';
 var xhttp = new XMLHttpRequest();
 xhttp.onreadystatechange = function() {
@@ -7,18 +7,53 @@ xhttp.onreadystatechange = function() {
        // Typical action to be performed when the document is ready:
        //document.getElementById("").innerHTML = xhttp.responseText;
        var newsapi = JSON.parse(xhttp.responseText);
-       var artigo = '';
-       for(var i = 0; i < 4; i++){
-        artigo += 
+       var linha1 = '';
+       var linha2 = '';
+       var linha3 = '';
+       var cont = 0;
+       for(var i = 0; i < 20; i++){
+        if( cont < 2){
+            linha1 += 
             '<div class="card-g">'+
-                '<img src="' +newsapi.articles[i].urlToImage+ '" alt="">'+
-                '<h4>' +newsapi.articles[i].publishedAt+'</h4>'+
-                '<h3>' +newsapi.articles[i].title+ '</h3>'+
+                '<div id="container-img">'+
+                    '<img src="' +newsapi.articles[i].urlToImage+ '" alt="">'+
+                '</div>'+
+                '<p>' +newsapi.articles[i].publishedAt+'</p>'+
+                '<h3><a href="'+ newsapi.articles[i].url +'" target="_blank">' +newsapi.articles[i].title+ '<a></h3>'+
                 '<p>' +newsapi.articles[i].description+ '</p>'+
-                '<h3>POR: ' +newsapi.articles[i].author+ '</h3>'+
-            '</div>';
+                '<p>POR : ' +newsapi.articles[i].author+ '</p>'+
+            '</div><br/>';
+            cont++;
+            }else if(cont < 5){
+                linha2 +=
+                '<div class="card-g">'+
+                '<div id="container-img">'+
+                    '<img src="' +newsapi.articles[i].urlToImage+ '" alt="">'+
+                '</div>'+
+                '<p>' +newsapi.articles[i].publishedAt+'</p>'+
+                '<h3><a href="'+ newsapi.articles[i].url +'" target="_blank">' +newsapi.articles[i].title+ '<a></h3>'+
+                '<p>' +newsapi.articles[i].description+ '</p>'+
+                '<p>POR : ' +newsapi.articles[i].author+ '</p>'+
+            '</div><br/>';
+            cont++;
+            }else if(cont < 7){
+                linha3 +=
+                '<div class="card-g">'+
+                '<div id="container-img">'+
+                    '<img src="' +newsapi.articles[i].urlToImage+ '" alt="">'+
+                '</div>'+
+                '<p>' +newsapi.articles[i].publishedAt+'</p>'+
+                '<h3><a href="'+ newsapi.articles[i].url +'" target="_blank">' +newsapi.articles[i].title+ '<a></h3>'+
+                '<p>' +newsapi.articles[i].description+ '</p>'+
+                '<p>POR : ' +newsapi.articles[i].author+ '</p>'+
+            '</div><br/>';
+            cont++;
+            }
+                document.getElementById("linha1").innerHTML = linha1;
+                document.getElementById("linha2").innerHTML = linha2;
+                document.getElementById("linha3").innerHTML = linha3;
+                
        }
-       document.getElementById("artigo-g").innerHTML = artigo;
     }
 };
 
